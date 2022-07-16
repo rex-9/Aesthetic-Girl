f7ffe6ade337cf965b7e2daf5b24fc5ba654a356
Htet Naing <htetnaing0814@gmail.com>
2022-07-16 19:34:36 +0630

    product selector

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import loadCards from './modules/dom.js'
+import products from './modules/products.js';
+import { loadCards, select } from './modules/dom.js'
+
+localStorage.setItem('products', JSON.stringify(products));
 
 loadCards();
+
+const selector = document.getElementById('selector');
+
+selector.addEventListener('change', (e) => {
+    select(selector.value)
+    e.preventDefault();
+});
--- modules/dom.js
+++ modules/dom.js
@@ -1,14 +1,41 @@
 import products from './products.js';
 
+const select = (type) => {
+    if (type !== "All Items") {
+        let selectedProducts = products.filter((product) => product.type === type);
+        let productsToHide = products.filter((product) => product.type !== type);
+        selectedProducts.forEach(product => {
+            const element = document.getElementById(product.id);
+            element.classList.add('block');
+            element.classList.remove('hidden');
+            console.log(element);
+        })
+        productsToHide.forEach(product => {
+            const element = document.getElementById(product.id);
+            element.classList.add('hidden');
+            element.classList.remove('block');
+            console.log(element);
+        })
+    } else {
+        products.forEach(product => {
+            const element = document.getElementById(product.id);
+            element.classList.add('block');
+            element.classList.remove('hidden');
+            console.log(element);
+        })
+    }
+}
+
 const cardContainer = document.getElementById('card-container');
 const loadCards = () => {
     products.forEach(product => {
         const card = document.createElement('div');
-        card.classList.add('bg-white', 'rounded-lg', 'm-2', 'cursor-pointer', 'shadow-lg', 'hover:shadow-blue-500/40', 'hover:ease-linear', 'hover:duration-500', 'hover:scale-105')
+        card.id = product.id;
+        card.classList.add('bg-white', 'rounded-lg', 'm-2', 'cursor-pointer', 'shadow-lg', 'hover:shadow-pink-500/40', 'hover:ease-linear', 'hover:duration-500', 'hover:scale-105')
         card.innerHTML = `<a href="./pages/info.html" class='relative'>
                             <img src='${product.img}' class='z-0 object-cover w-56 h-40 rounded-t-lg'>
-                            <p class="text-center font-bold pt-1 w-56">
-                                ${product.name}
+                            <p class="text-center text-lg font-bold pt-1 w-56">
+                               ${product.id}. ${product.name}
                             </p>
                             <div class="flex justify-between items-start px-2 pb-1">
                                 <p class="text-green-500 font-bold">
@@ -26,4 +53,7 @@ const loadCards = () => {
     });
 }
 
-export default loadCards;
+export {
+    loadCards,
+    select
+};
